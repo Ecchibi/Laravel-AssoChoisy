993c46b22dfbd67af34e2cacc6617e0b9adc34b4
youba <youba.imakhlaf@gmail.com>
2022-02-15 11:46:39 +0100

    plus de gitignore

--- app/Http/Controllers/administrer.php
+++ app/Http/Controllers/administrer.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class administrer extends Controller
 {
-    //
+    function administrer(){        
+     
+        // PdoAssoChoisy::getTitreActivites(1);
+        
+        return view('vu_activites');
+        // ->with('visiteur',$visiteur); 
+    }
 }
