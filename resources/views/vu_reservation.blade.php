19ea6f893bf8443caf4d45e2ea15245a48cadb8f
percybazile <percybazile@gmail.com>
2022-02-18 10:39:18 +0100

    Modif du reserv

--- resources/views/vu_reservation.blade.php
+++ resources/views/vu_reservation.blade.php
@@ -37,20 +37,13 @@
 						<div class="booking-bg">
 							<div class="form-header">
 								<h2>Reserver une branche horaires pour être rappeler</h2>
-								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate laboriosam numquam at</p>
 							</div>
 						</div>
 						<form>
 							<div class="row">
 								<div class="col-md-6">
 									<div class="form-group">
-										<span class="form-label">Check In</span>
-										<input class="form-control" type="date" required>
-									</div>
-								</div>
-								<div class="col-md-6">
-									<div class="form-group">
-										<span class="form-label">Check Out</span>
+										<span class="form-label">Date</span>
 										<input class="form-control" type="date" required>
 									</div>
 								</div>
@@ -58,38 +51,43 @@
 							<div class="row">
 								<div class="col-md-6">
 									<div class="form-group">
-										<span class="form-label">Adults</span>
-										<select class="form-control">
-											<option>1</option>
-											<option>2</option>
-											<option>3</option>
-										</select>
-										<span class="select-arrow"></span>
+										<span class="form-label">Nom :</span>
+										 <input class="form-control" type="nom" required>
 									</div>
 								</div>
 								<div class="col-md-6">
 									<div class="form-group">
-										<span class="form-label">Children</span>
-										<select class="form-control">
-											<option>0</option>
-											<option>1</option>
-											<option>2</option>
-										</select>
-										<span class="select-arrow"></span>
+										<span class="form-label">Prenom :</span>
+            							<input class="form-control" type="prenom" required>
 									</div>
 								</div>
 							</div>
-							<div class="form-group">
-								<span class="form-label">Room Type</span>
-								<select class="form-control" required>
-									<option value="" selected hidden>Select room type</option>
-									<option>Private Room (1 to 2 People)</option>
-									<option>Family Room (1 to 4 People)</option>
-								</select>
-								<span class="select-arrow"></span>
-							</div>
-							<div class="form-btn">
-								<button class="submit-btn">Check availability</button>
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+										<span class="form-label">Organisme :</span>
+										 <input class="form-control" type="organisme">
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<span class="form-label">Numero de Téléphone :</span>
+            							<input class="form-control" type="tel" required>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<span class="form-label">Adresse mail :</span>
+            							<input class="form-control" type="mail" required>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<span class="form-label">Lieu de la prestation :</span>
+            							<input class="form-control" type="lieu" required>
+									</div>
+								</div>
+								<button class="submit-btn">Valider</button>
 							</div>
 						</form>
 					</div>
