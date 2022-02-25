1e28419f6d7a718ae7efce43b704425354484e3d
youba <youba.imakhlaf@gmail.com>
2022-02-25 10:41:00 +0100

    ajout lien ajouter a vu_connexion

--- resources/views/vu_connexion.blade.php
+++ resources/views/vu_connexion.blade.php
@@ -3,7 +3,10 @@
     
            <h1><img src="{{ asset('img/logo_First.png') }}" alt="logo du site"></h1>
     <link rel="stylesheet" href="{{ asset('css/connexion.css') }}">
-     
+    <div class="lienAccueil"> 
+        <a href="{{ route('chemin_accueil')}}">Accueil </a> 
+    </div>
+
     <form action="{{ route('chemin_connecter') }}" method="post">
         {{ csrf_field() }} <!-- laravel va ajouter un champ caché avec un token -->
         <h1>Connexion </h1>
