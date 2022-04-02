93cd35e3898a0d519195869cfb249b06fad2f4a4
youba <youba.imakhlaf@gmail.com>
2022-04-02 16:18:24 +0200

    edit

--- resources/views/vu_banniere.blade.php
+++ resources/views/vu_banniere.blade.php
@@ -3,7 +3,7 @@
 <!---------------------------------- BANNIERE ---------------------------->
 <!-- slider_area_start -->
 <div class="slider_area">
-    <div class="single_slider  d-flex align-items-center slider_bg_1 overlay2">
+    <div class="single_slider d-flex align-items-center slider_bg_1 overlay2">
         <div class="container">
             <div class="row">
                 <div class="col-lg-9">
